d94e2a0d888cae25f121425eb30056e7a9c6f449
azaharafernandezguizan <azaharafernandezguizan@gmail.com>
2017-08-27 15:55:48 +0200

    changes in loadMatrix

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -82,25 +82,34 @@ function loadMatrizBoard(imagesNumber) {
 }
 
 function paintBoard() {
-    // Obtener la referencia del elemento body
+
     var body = document.getElementsByTagName("body")[0];
 
-    // Crea un elemento <table> y un elemento <tbody>
     var tabla = document.createElement("table");
     var tblBody = document.createElement("tbody");
 
     // Crea las celdas
     for (var i = 0; i < 2; i++) {
-        // Crea las hileras de la tabla
+       
         var hilera = document.createElement("tr");
 
         for (var j = 0; j < 2; j++) {
-            // Crea un elemento <td> y un nodo de texto, haz que el nodo de
-            // texto sea el contenido de <td>, ubica el elemento <td> al final
-            // de la hilera de la tabla
             var celda = document.createElement("td");
-            var textoCelda = document.createTextNode("celda en la hilera " + i + ", columna " + j);
-            celda.appendChild(textoCelda);
+
+            var btn = document.createElement('input');
+            btn.type = "button";
+            btn.className = "btnBoard";
+            btn.id = "btnBoard"+matrizBoard[i][j];
+            btn.onclick = (function(this) {return function() {testAction(this);}})(this);
+
+            var img = document.createElement('img');
+            img.src = "images/"+matrizBoard[i][j]+"jpg";
+            img.alt = "imagen"+matrizBoard[i][j];
+            img.title = "imagen para emparejar"+matrizBoard[i][j];
+            img.style ="display:none;"
+            
+            celda.appendChild(btn);
+            celda.appendChild(img);
             hilera.appendChild(celda);
         }
 
@@ -114,4 +123,8 @@ function paintBoard() {
     body.appendChild(tabla);
     // modifica el atributo "border" de la tabla y lo fija a "2";
     tabla.setAttribute("border", "2");
+}
+
+function testAction(entry){
+    
 }
